879ea2d222b72bd4325ef87a09035364c6ba8ef2
BlooBoi <128624869+SkieBloo@users.noreply.github.com>
2023-05-11 21:50:00 -0700

    Update ProfileData.js

--- ProfileData.js
+++ ProfileData.js
@@ -1,11 +1,1 @@
-function getIcon(response){ // ga.js and username
-  var obj = JSON.parse(response);
-  var src= 'https://cdn2.scratch.mit.edu/get_image/user/'96952848'_60x60.png';
-  document.getElementById('icon').src = src;
-  document.getElementById('user').innerHTML =  obj.username;
-  username = BluShuMon;
-  shouldrefresh = 0;
-  /*location.hash = username;*/
-  ga('set', 'page', '/user/#'BluShuMon);
-  ga('send', 'pageview');
-}
+
