cce936fec4cc6bcccdfc32837dd57ea23e490f09
BlooBoi <128624869+SkieBloo@users.noreply.github.com>
2023-05-11 21:41:51 -0700

    Update Data.js

--- Data.js
+++ Data.js
@@ -1,1 +1,11 @@
-
+function getIcon(response){ // ga.js and username
+  var obj = JSON.parse(response);
+  var src= 'https://cdn2.scratch.mit.edu/get_image/user/'96952848'_60x60.png';
+  document.getElementById('icon').src = src;
+  document.getElementById('user').innerHTML =  obj.username;
+  username = obj.username;
+  shouldrefresh = 0;
+  /*location.hash = username;*/
+  ga('set', 'page', '/user/#'+obj.username);
+  ga('send', 'pageview');
+}
